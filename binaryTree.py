f986a9669c2ce18e885e1c988839fb0a64d11b74
esksw4 <e.kim4.backup@gmail.com>
2017-04-24 20:05:39 -0400

    binaryTree

--- binaryTree.py
+++ binaryTree.py
@@ -1,25 +1,9 @@
-https://www.youtube.com/watch?v=H5JubkIy_p8
-
-Bianry Tree: 
-- each node can have AT MOST two children
-- ANY Tree is Binary tree if each node does not contain more than two children.
-- Leaf Node: all left and right children are NULL
-
-Complete Binary Tree:
-- All levels except possibly the last are completly filled and all nodes are as left as possible
-- max number of nodes at level i = 2^i
-- height : 
-
-Perfect Binary Tree:
-- All levels are completely filled
-- max number of nodes with height h = 2^(number of levels) - 1
-- height with n number of nodes: h = log_2 (n+1) - 1
-
-Balanced Binary Tree:
-- Difference between height of left and right subtree for every node is not more than 1.
-
-Binary Tree can be implemented using:
-a) dynamically created nodes : Pointers
-b) Arrays (mostly for complete binary tree)
-
-Binary Search Tree:
+import sys
+def check(root, min, max):
+    if root == None:
+        return True
+    if root.data <= min or root.data >= max:
+        return False
+    return check(root.left, min, root.data) and check(root.right, root.data, max)
+def check_binary_search_tree_(root):
+    return check(root, float('-inf'), float('inf'))
